c47ae62d27ba7ac088997a7619191cb5d2272eb8
ReubenJCarter <reubenjcarter@aol.com>
2012-05-27 10:31:21 +0100

    added waterWaves.fs testProgram

---
+++ FragmentCommandLine/build/testPrograms/WaterWaves.fs
@@ -0,0 +1,12 @@
+void main()
+{
+  vec4 color;
+  vec2 c1 = vec2(Coord().x - 0.2, Coord().y - 0.2 );
+  vec2 c2 = vec2(Coord().x - 0.8, Coord().y - 0.8);
+  float sin1 = (1.0 / (length(c1) * 3.0) ) * sin(length(c1) * 2.0 * 3.14159 * 20.0)/2.0 +0.5;
+  float sin2 = (1.0 / (length(c2) * 3.0) ) * sin(length(c2) * 2.0 * 3.14159 * 30.0)/2.0 +0.5;
+
+  color = vec4((sin1 + sin2) / 2.0, 0.0, 0.0, 1.0);
+  
+  gl_FragColor = color;
+}
